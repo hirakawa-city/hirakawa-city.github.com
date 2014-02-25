57cc60efccd19e5299cfe105d79831c7abf66e2e
questb <quest.b@gmail.com>
2014-02-25 17:23:53 +0900

    Update dailybread.js

--- js/dailybread.js
+++ js/dailybread.js
@@ -194,7 +194,7 @@ OpenSpending.DailyBread = function (elem) {
       var iconUrl, paper;
       iconUrl = $(e).data('svg-url');
       paper = Raphael(e, iconRad+iconRad,iconRad+iconRad+5);
-      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#A5D148', stroke: 'none' });
+      paper.circle(iconRad,iconRad,iconRad).attr({ fill: '#007B49', stroke: 'none' });
       paper.circle(iconRad,iconRad,iconRad-2).attr({ fill: 'none', stroke: '#eee', opacity: .8, 'stroke-dasharray': '- ' });
       $.get(iconUrl, function(svg) {
         if (typeof(svg) == "string") {
